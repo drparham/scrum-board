32d9431490305075a21ccbcfcc3023d9eb29f9eb
Dustin Parham <dr.parham@gmail.com>
2017-06-01 18:37:49 -0600

    add package service providers to app config

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
+        Rinvex\Repository\Providers\RepositoryServiceProvider::class,
+        Silber\Bouncer\BouncerServiceProvider::class,
 
     ],
 
@@ -225,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Bouncer' => Silber\Bouncer\BouncerFacade::class,
     ],
 
 ];
