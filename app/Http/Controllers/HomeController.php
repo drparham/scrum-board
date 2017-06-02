2d7ff08f2914b8046f714ea3b4ca42d8bb3472cb
Dustin Parham <dr.parham@gmail.com>
2017-06-02 10:15:41 -0600

    update HomeController and home blade to have orgs and projects

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+
+        $organizations = $user->organizations;
+        $projects      = $user->projects;
+
+        return view('home')->with(['organizations' => $organizations, 'projects' => $projects]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,50 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Organization Projects</div>
-
-                <div class="panel-body">
-                    Show projects that belong to Organizations user is a member of.
+            @if (!$organizations->isEmpty())
+                <div class="panel panel-default">
+                    <div class="panel-heading">Organization Projects</div>
+                    <div class="panel-body">
+                        @foreach($organizations as $organization)
+                            <div class="panel-content">
+                                <a href="#">{{$organization->name}}</a>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading">User Projects</div>
-
-                <div class="panel-body">
-                    Show Projects that belong to user.
+            @else
+                <div class="panel panel-default">
+                    <div class="panel-heading">Organization Projects</div>
+                    <div class="panel-body">
+                        <div class="panel-content">
+                            You do not belong to any organizations, would you like to <a href="#">create</a> one?
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endif
 
-
+            @if (!$projects->isEmpty())
+                <div class="panel panel-default">
+                    <div class="panel-heading">User Projects</div>
+                    <div class="panel-body">
+                        @foreach($projects as $project)
+                            <div class="panel-content">
+                                <a href="#">{{$project->name}}</a>
+                                {{$project->description}}
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            @else
+                <div class="panel panel-default">
+                    <div class="panel-heading">User Projects</div>
+                    <div class="panel-body">
+                        <div class="panel-content">
+                            You have no projects, would you like to <a href="#">create</a> one?
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
