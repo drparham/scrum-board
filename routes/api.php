530b93535321ad5c3576f0eb453997d52f063b45
Dustin Parham <dr.parham@gmail.com>
2017-06-02 11:19:50 -0600

    add routes for organization api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/api/v1/org/create', 'App\Http\Controllers\Api\V1\OrganizationController@create');
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/organization/{id}', 'OrganizationController@index')->name('organization');
+Route::get('/organization/{id}', 'OrganizationController@index')->name('organizations');
+Route::middleware('auth')->get('/api/v1/org/create', 'Api\V1\OrganizationController@create')->name('createOrg');
+Route::middleware('auth')->get('/api/v1/org/edit', 'Api\V1\OrganizationController@edit')->name('editOrg');
+Route::middleware('auth')->post('/api/v1/org/store', 'Api\V1\OrganizationController@store')->name('storeOrg');
+Route::middleware('auth')->post('/api/v1/org/update', 'Api\V1\OrganizationController@update')->name('updateOrg');
+Route::middleware('auth')->delete('/api/v1/org/delete', 'Api\V1\OrganizationController@delete')->name('deleteOrg');
